af788f4b627f6d73ca121110eb291e57592ecf02
jonathanfleming135 <jonathanfleming135@gmail.com>
2018-08-25 18:22:26 -0700

    Added shimmer function

--- src/rpi/patterns/shimmer.py
+++ src/rpi/patterns/shimmer.py
@@ -1,17 +1,26 @@
 import arduino_control as arduino
 import constants as const
-import random
+import random as rand
 
-def pattern():
-	print("hellotesthello")
+def pattern(run_time):
+	run_time = run_time * 1000.0
+	start_time = arduino.clock()
 
-	#led_write_time = 200
-	#leds_per_write = arduino.max_leds_per_write(led_write_time)
-#
-	#leds = []
-	#for led in range(0, leds_per_write):
-	#	curr_led = random.randint(0, const.ALL_LEDS)
-	#	if ((curr_led, const.WHITE) not in leds):
-	#		leds.append((curr_led, const.WHITE))
-#
-	#print(leds)
+	num_leds = round(const.ALL_LEDS / 10.0)
+	time_between_writes = 100
+
+	num_leds = min(arduino.max_lines_per_write(time_between_writes), num_leds)
+
+	while (start_time + run_time > arduino.clock() ):
+		arduino.clear_leds()
+
+		LEDs = []
+		for led in range (0, num_leds):
+			curr_led = rand.randint(0, const.ALL_LEDS)
+			if ((curr_led, const.WHITE) not in LEDs):
+				LEDs.append((curr_led, const.WHITE))
+
+		write_time = arduino.clock()
+		arduino.write_packet(LEDs)
+		time_already_waited = arduino.clock() - write_time
+		arduino.sleep(time_between_writes - time_already_waited)
