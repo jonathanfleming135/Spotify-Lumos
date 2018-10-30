4e2adf5dc44172a3dd4cfbfecd2cd586c3caded2
Jonathan Fleming <jonathanfleming135@gmail.com>
2018-10-29 21:37:37 -0700

    finished initial part of spotify auth

--- src/host/spotify_auth.py
+++ src/host/spotify_auth.py
@@ -1,29 +1,22 @@
 #!/usr/bin/python
 
 import webbrowser
-import time
 import socket
-import subprocess
 import re
 import datetime
 
-
-id_file = open("client_id.txt", "r")
+# get the client id and secret from metadata
+id_file = open("../../metadata/client_id.txt", "r")
 id_str = id_file.read()
 id_file.close()
-
 client_id, client_secret = id_str.split(",")
 
-
-
-local_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-local_socket.bind(('127.0.0.1', 8080))
-local_socket.listen()
-
+# read html file for response
 html_f = open("you_are_now_connected.html", 'r')
 html = html_f.read()
 html_f.close()
 
+# build GET HTTP request for spotify
 url_params = {
 	"auth_url": "https://accounts.spotify.com/authorize/",
 	"client_id":  "client_id={0}".format(client_id),
@@ -36,11 +29,10 @@ url += "?" + url_params["client_id"]
 url += "&" + url_params["redirect"]
 url += "&" + url_params["response"]
 
-webbrowser.open_new_tab(url)
-
+# build response for local redirect
 res_params = {
 	"HTTP": "HTTP/1.1 200 OK\n",
-	"Date": "Date: {0}\n".format(str(datetime.datetime.now())),#Wed, 11 Apr 2012 21:29:04 GMT\n",#{0}\n".format(str(datetime.datetime.now())),
+	"Date": "Date: {0}\n".format(str(datetime.datetime.now())),
 	"Server": "Server: Python/6.6.6 (custom)\n",
 	"Connection": "Connection: closed\n",
 	"Content-Type": "Content-Type: text/html \n\n",
@@ -55,23 +47,28 @@ response += res_params["Connection"]
 response += res_params["Content-Type"]
 response += res_params["html"]
 
+# open socket on local host
+local_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+local_socket.bind(('127.0.0.1', 8080))
+local_socket.listen()
 
+# send request to spotify via browser
+webbrowser.open_new_tab(url)
+
+# listen for response on socket
 connection, addr = local_socket.accept()
 while True:
 	data = connection.recv(1024)
 	if (data):
-		print(data)
 		connection.send(response.encode())
 		local_socket.close()
 		connection.close()
 		break
 
+# parse response and write token to file
 regex = re.search('code=(\S+)', data.decode())
 token = regex.group(1)
 
 token_file = open("../../metadata/token.txt", "w+")
 token_file.write(token)
 token_file.close()
-
-# may not be necessary since sshfs
-#bashCommand = "scp {0} lumos:~/spotify-lumos/metadata"
