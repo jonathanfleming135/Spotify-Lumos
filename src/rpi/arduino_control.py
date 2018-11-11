adb56adfbaed9cd90f757056efaceffa85b0e6e2
Jonathan Fleming <jonathanfleming135@gmail.com>
2018-11-10 18:46:52 -0800

    get song features and analysis api calls working - api stuff needs testing

--- src/rpi/arduino_control.py
+++ src/rpi/arduino_control.py
@@ -4,23 +4,29 @@ import time
 import constants as const
 import utils as util
 
-# Initializes Serial port for communication with Arduino
 def init_port():
+    '''
+    Initializes Serial port for communication with Arduino
+    '''
     global port
     port = serial.Serial('/dev/ttyACM0', 115200)
 
-# Writes a list of led values to the Arduino, waits till the next available led
-# write time without stopping the clock
-#
-# @param[in]  LED_values
 def write_packet(LED_values):
+    '''
+    Writes a list of led values to the Arduino, waits till the next available led
+    write time without stopping the clock
+
+    @param[in]  LED_values
+    '''
     packet = encode_packet(LED_values)
     port.write(packet.encode())
     sleep_time = util.time_to_next_write(len(LED_values))
     util.sleep(sleep_time)
 
-# Encodes a packet to send to the Arduino
 def encode_packet(LED_values):
+    '''
+    Encodes a packet to send to the Arduino
+    '''
     packet = ""
     for LED in LED_values:
         packet += str(LED[0])
--- src/rpi/spotify_api_requests.py
+++ src/rpi/spotify_api_requests.py
@@ -13,23 +13,126 @@ get_current_song_endpoint =  "https://api.spotify.com/v1/me/player/currently-pla
 get_song_analysis_endpoint = "https://api.spotify.com/v1/audio-analysis/"
 get_song_features_endpoint = "https://api.spotify.com/v1/audio-features/"
 
+# song timing
+curr_song_end_time = 0.0
+uri = ""
+
+# song analysis
+song_features = {}
+song_analysis = {}
+
 def get_currently_playing_song():
 	'''
 	This method will be used like a "private" method for this file,
-	it should not be called by methods outside this file
+	it should not be called by methods outside this file.
+
+	All times expressed in seconds as a float
+
+	returns the uri of the song being currently played
 	'''
 
-	params = {
+	global token
+	global curr_song_end_time
+	global uri
+
+	if (time.clock() < curr_song_end_time):
+		return uri
+	else:
+		headers = {
+			"Authorization": "Bearer {0}".format(token)
+		}
+
+		req = requests.get(get_current_song_endpoint, headers=headers)
+		req = req.json()
+
+		progress = float(req["progress_ms"]) / 1000.0
+		duration = float(req["item"]["duration_ms"]) / 1000.0
+		curr_song_end_time = (duration - progress) + time.clock()
+
+		uri = req["item"]["uri"].split(":")[-1]
+		return uri
+
+def check_currently_playing_song():
+	'''
+	This method should be called every 3 seconds - to check the currently
+	playing song hasn't changed (in the case a user manually selects a
+	different song)
+	'''
+
+	global token
+	global uri
+
+	headers = {
 		"Authorization": "Bearer {0}".format(token)
 	}
 
-	req = requests.get(get_current_song_endpoint, headers=params)
-	print(req.text)
+	req = requests.get(get_current_song_endpoint, headers=headers)
+	req = req.json()
 
-def get_song_features(song_uri):
-	pass
+	curr_song_uri = req["item"]["uri"].split(":")[-1]
+	if (uri != curr_song_uri):
+		uri = curr_song_uri
+		#TODO - call method to recalculate patterns for a song
 
-def get_song_analysis(song_uri):
-	pass
+def get_song_features():
+	'''
+	This function returns a json object of the spotify song features
+	api corresponding to the currently playing song
 
-get_currently_playing_song()
+	It is meant to be called anywhere in the code
+	'''
+
+	global token
+	global uri
+	global song_features
+
+	prev_uri = uri
+	curr_uri = get_currently_playing_song()
+
+	if (curr_uri == prev_uri):
+		return song_features
+	else:
+		endpoint = "{0}{1}".format(get_song_features_endpoint, uri)
+
+		headers = {
+			"Authorization": "Bearer {0}".format(token)
+		}
+
+		req = requests.get(endpoint, headers=headers)
+		req = req.json()
+
+		song_features = req
+		return req
+
+
+def get_song_analysis():
+	'''
+	This function returns a json object of the spotify song analysis
+	api corresponding to the currently playing song
+
+	It is meant to be called anywhere in the code
+	'''
+	global token
+	global uri
+	global song_features
+
+	prev_uri = uri
+	curr_uri = get_currently_playing_song()
+
+	if (curr_uri == prev_uri):
+		return song_features
+	else:
+		endpoint = "{0}{1}".format(get_song_analysis_endpoint, uri)
+
+		headers = {
+			"Authorization": "Bearer {0}".format(token)
+		}
+
+		req = requests.get(endpoint, headers=headers)
+		print(req.text)
+		req = req.json()
+
+		song_features = req
+		return req
+
+get_song_analysis()
