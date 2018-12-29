ea628545747a2a17aae94a5eb51a2f6c59c5ccbe
Jonathan Fleming <jonathanfleming135@gmail.com>
2018-12-29 12:20:44 -0800

    added pitch colour to volume - works but needs heavy code cleanups

--- src/rpi/patterns/volume.py
+++ src/rpi/patterns/volume.py
@@ -4,6 +4,9 @@ import random as rand
 import time
 import utils as util
 import spotify_api_requests as spot_api
+# import pitch
+
+COLOURS_PER_PITCH = const.NUM_COLOURS / const.NUM_PITCHES
 
 def pattern(progress, duration):
 	'''
@@ -33,10 +36,13 @@ def pattern(progress, duration):
 		num_leds = int(round(loudness_percent * (const.CORNER_ONE - const.CORNER_ZERO)))
 		LEDs = []
 		opposite_led = const.CORNER_THREE - num_leds
-		LEDs.append((const.CORNER_ZERO, const.PURPLE, num_leds))
+
+		pitches = curr_segment["pitches"]
+		colour = get_colour_from_pitches(pitches)
+
+		LEDs.append((const.CORNER_ZERO, colour, num_leds))
 		LEDs.append((num_leds, const.BLACK, const.CORNER_TWO + (const.CORNER_ONE - num_leds) ))
-		LEDs.append((const.CORNER_TWO + (const.CORNER_ONE - num_leds), const.PURPLE, const.CORNER_THREE))
-		LEDs.append((const.CORNER_THREE, const.PURPLE, const.CORNER_FOUR))
+		LEDs.append((const.CORNER_TWO + (const.CORNER_ONE - num_leds), colour, const.CORNER_FOUR))
 		arduino.write_packet(LEDs)
 
 		progress += segments[0]["duration"]
@@ -67,3 +73,49 @@ def get_min_loudness(segments):
 		if (curr_loudness < min_loudness):
 			min_loudness = curr_loudness
 	return min_loudness
+
+def get_pitch_index(pitches):
+	'''
+	Helper function to retrieve the current pitch
+
+	@param pitches - the list of pitches as return from the spotify api request
+	@return pitch index (corresponds to pitch of the note being played)
+	'''
+	for i in range(0, len(pitches)):
+		if (pitches[i] == 1.0):
+			return i
+
+def get_colour_from_pitches(pitches):
+	'''
+	Get's the colour corresponding to the pitch of the sound
+
+	@param pitches - the list of pitches as return from the spotify api request
+	@return colour
+	'''
+
+	pitch_index = get_pitch_index(pitches)
+
+	pitch_index_above = (pitch_index + 1) % (const.NUM_PITCHES - 1)
+	pitch_index_below = (pitch_index - 1) % (const.NUM_PITCHES - 1)
+
+	pitch_above = pitches[pitch_index_above]
+	pitch_below = pitches[pitch_index_below]
+
+	colour = pitch_index * COLOURS_PER_PITCH
+
+	if (pitch_above > pitch_below):
+		colour_diff = (COLOURS_PER_PITCH / 2.0) * pitches[pitch_index_above]
+		colour += colour_diff
+	elif (pitch_below > pitch_above):
+		colour_diff = (COLOURS_PER_PITCH / 2.0) * pitches[pitch_index_below]
+		colour -= colour_diff
+	else:
+		colour = COLOURS_PER_PITCH * pitch_index
+
+	colour = round(colour)
+
+	# avoid using special colour values
+	if (colour == 0 or colour == 255):
+		colour += 1
+
+	return colour
